92a3ee29748b30b12d3c854800678b6d13baf613
muralikarthik123 <160646378+muralikarthik123@users.noreply.github.com>
2025-11-23 16:56:17 +0530

    services.jsx services.css App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Header from './header/header'
 import Footer from './footer/footer'
 import Home from './home/home.jsx'
 import About from './about/about.jsx'
-// import Services from './service/service.jsx'
+import Services from './services/services.jsx'
 
 import {  BrowserRouter, Route,Routes } from 'react-router-dom'
 export default function App() {
@@ -14,7 +14,7 @@ export default function App() {
         <Routes>
           <Route path='/' element={<Home/>}/>
           <Route path='/about' element={<About/>}/>
-          {/* <Route path='/services' element={<Service/>}/> */}
+          <Route path='/services' element={<Services/>}/>
         </Routes>
         <Footer />
       </BrowserRouter>
--- src/services/services.jsx
+++ src/services/services.jsx
@@ -1,9 +1,50 @@
 import React from 'react'
-
+import './services.css'
 export default function services() {
   return (
-    <div>
-      <p>Services</p>
+    <div className='services'>
+      <h2 className='heading'>Our Services</h2>
+      <p className='headingcontent'>Our company provides a wide range of services tailored to meet your unique needs. From cutting-edge technology solutions to exceptional customer support, we're dedicated to delivering excellence at every turn.</p>
+
+      {/* main box1 */}
+      <div className='box'>
+        {/* sub box 1 */}
+        <div className='subox'>
+          <h2>Web Development </h2>
+          <p>Our web development team creates user-friendly, responsive websites,Our web development team creates user-friendly, responsive websites.</p>
+        </div>
+
+        {/* sub box 2 */}
+        <div className='subox'>
+          <h2>Cloud services</h2>
+          <p>Our web development team creates user-friendly, responsive websites,Our web development team creates user-friendly, responsive websites.</p>
+        </div>
+
+        {/* sub box 3 */}
+        <div className='subox'>
+          <h2>Mobile Development </h2>
+          <p>Our web development team creates user-friendly, responsive websites,Our web development team creates user-friendly, responsive websites.</p>
+        </div>
+
+      </div>
+
+      {/* main box2 */}
+      <div className='box'>
+        <div className='subox'>
+          <h2>QA & testing </h2>
+          <p>Our web development team creates user-friendly, responsive websites,Our web development team creates user-friendly, responsive websites.</p>           
+        </div>
+        <div className='subox'>
+          <h2>IT support & maintenance </h2>   
+          <p>Our web development team creates user-friendly, responsive websites,Our web development team creates user-friendly, responsive websites.</p>            
+        </div>
+
+        <div className='subox'>
+          <h2>Add website design</h2>   
+          <p>Our web development team creates user-friendly, responsive websites,Our web development team creates user-friendly, responsive websites.</p>            
+        </div>
+
+      </div>  
     </div>
   )
 }
